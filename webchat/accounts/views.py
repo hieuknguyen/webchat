5411048cbc596d7191db0a3f73906484076c3d85
Anonymous-204 <nladuong.2004@gmail.com>
2025-05-28 05:29:30 +0700

    cap nhat view accounts

--- webchat/accounts/views.py
+++ webchat/accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+
+def login(request):
+    return render(request,'login.html')
+def register(request):
+    return render(request,'register.html')
+def changepassword(request):
+    return render(request,'changepassword.html')
+# Create your views here.
